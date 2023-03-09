356cf62a6e157a4e1c44dc43ac7a81f475ea1eb4
GabrielEstevesDev <112899019+GabrielEstevesDev@users.noreply.github.com>
2023-03-09 10:39:42 +0100

    fonction accueil

--- econtact/src/Controller/UtilisateurController.php
+++ econtact/src/Controller/UtilisateurController.php
@@ -15,6 +15,8 @@ use Doctrine\ORM\EntityManagerInterface;
 
 class UtilisateurController extends AbstractController
 {
+    public static $res;
+    public static $tab=array();
     #[Route('/utilisateur', name: 'app_utilisateur')]
     public function index(): Response
     {
@@ -27,33 +29,36 @@ class UtilisateurController extends AbstractController
 
     public function ident(Request $request, EntityManagerInterface $entityManager){
         $utilisateur=new Utilisateur();
-
         $form = $this->createForm(FormUtilisateurType::class,$utilisateur);
 
         $form->handleRequest($request);
 
         if($form->isSubmitted()&& $form->isValid()){
-            $res = $entityManager->getRepository(Utilisateur::class)->findOneBy([
+            self::$res = $entityManager->getRepository(Utilisateur::class)->findOneBy([
                 'nom' => $form["nom"]->getData(),
                 'num' => $form["num"]->getData()
             ]);
             //var_dump($res);
 
-            if($res==NULL){
+            if(self::$res==NULL){
                 $msg="Le compte n'existe pas";
+                return $this->render('utilisateur/index.html.twig',[
+                    'formconnexion'=>$form->createView(),
+                    'msg'=>$msg
+                ]);
             }
             else{
-                $query=$entityManager->getRepository(Contact::class)->findBy(['id_nom'=>$res->getIdNom()]);
-                $tab=array();
+                $query=$entityManager->getRepository(Contact::class)->findBy(['id_nom'=>self::$res->getIdNom()]);
+                // $tab=array();
                 //var_dump($query);
                 foreach($query as $value){
                     $contact=$entityManager->getRepository(Utilisateur::class)->findBy(['id_nom'=>$value->getIdContact()]);
-                    array_push($tab,$contact);
+                    array_push(self::$tab,$contact);
                 }
                 //var_dump($tab);
                 return $this->render('utilisateur/accueil.html.twig',[
-                    'nom'=>$res->getNom(),
-                    'liste'=>$tab
+                    'nom'=>self::$res->getNom(),
+                    'liste'=>self::$tab
                 ]); 
             }
         }
@@ -64,6 +69,14 @@ class UtilisateurController extends AbstractController
         ]);
 
 
+    }
+
+    #[Route('/accueil', name: 'home')]
+    public function accueil(){
+        return $this->render('utilisateur/accueil.html.twig',[
+            'nom'=>self::$res->getNom(),
+            'liste'=>self::$tab
+        ]); 
     }
 }
 ?>
